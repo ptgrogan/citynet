b8975d3de694e4ac069cf5b1b5675dadd5e7384d
Paul T. Grogan <pgrogan@stevens.edu>
2011-05-16 18:20:43 +0000

    added "hacked" implementation of polybool (relies on sampling the rectangular cell region) for users without the mapping toolpack

--- AbstractRegion.m
+++ AbstractRegion.m
@@ -30,9 +30,26 @@ classdef AbstractRegion < handle
         %   verticesX:  the x-coordinates of the vertices to check
         %   verticesY:  the y-coordinates of the vertices to check
         function out = ContainsVertices(obj,verticesX,verticesY)
-            [ix iy] = polybool('intersection',obj.verticesX,obj.verticesY,verticesX,verticesY);
-            out = sum(inpolygon(obj.verticesX,obj.verticesY,verticesX,verticesY))>0 || ...
-                polyarea(ix,iy)/polyarea(verticesX,verticesY) > SynthesisTemplate.instance().minIntersectionFraction;
+            try
+                [ix iy] = polybool('intersection',obj.verticesX,obj.verticesY,verticesX,verticesY);
+                out = sum(inpolygon(obj.verticesX,obj.verticesY,verticesX,verticesY))>0 || ...
+                    polyarea(ix,iy)/polyarea(verticesX,verticesY) > SynthesisTemplate.instance().minIntersectionFraction;
+            catch
+                % if the user does not have the mapping toolbox installed,
+                % the previous line will not execute... as a hack, let's
+                % estimate the intersection area by sampling the cell
+                % area... this requires that the verticesX and verticesY
+                % provided to this function describe a rectangle
+                num_samples = 16;
+                [samplesX,samplesY] = meshgrid( ...
+                    linspace(min(verticesX),max(verticesX),sqrt(num_samples)), ...
+                    linspace(min(verticesY),max(verticesY),sqrt(num_samples)));
+                samplesX = reshape(samplesX,num_samples,1);
+                samplesY = reshape(samplesY,num_samples,1);
+                intersectionFraction = sum(inpolygon(samplesX,samplesY,obj.verticesX,obj.verticesY))/num_samples;
+                out = sum(inpolygon(obj.verticesX,obj.verticesY,verticesX,verticesY))>0 || ...
+                    intersectionFraction > SynthesisTemplate.instance().minIntersectionFraction;
+            end
         end
     end
 end
