da19d2d5cc4e7535c405d521b6b94d9b0cd83850
Paul T. Grogan <pgrogan@stevens.edu>
2011-06-14 05:05:15 +0000

    added system property, removed id property (will not store in a flat database), added some protected utility functions

--- SystemBehavior.m
+++ SystemBehavior.m
@@ -7,22 +7,15 @@
 %%
 classdef SystemBehavior < Behavior
     properties
-        id;             % unique identifier of behavior, integer
+        system;     % the system handle to which this behavior belongs
     end
     methods
         %% Behavior Constructor
         % Creates a new instance of a Behavior object.
-        %        
-        % obj = SystemBehavior(id, name, description, units, bounds)
-        %   obj:            the new SystemBehavior instance
-        %   id:             identifier of the system behavior (integer)
-        %   name:           name of the behavior (string)
-        %   description:    description of the behavior (string)
-        %   units:          units of behavior value (string)
-        %   bounds:         allowable bounds on behavior value (string)
         %
-        % obj = SystemBehavior(name, description, units, bounds)
+        % obj = SystemBehavior(system, name, description, units, bounds)
         %   obj:            the new SystemBehavior instance
+        %   system:         the system handle to which this behavior belongs
         %   name:           name of the system behavior (string)
         %   description:    description of the behavior (string)
         %   units:          units of behavior value (string)
@@ -37,11 +30,6 @@ classdef SystemBehavior < Behavior
                 args{2} = varargin{3};
                 args{3} = varargin{4};
                 args{4} = varargin{5};
-            elseif nargin==4
-                args{1} = varargin{1};
-                args{2} = varargin{2};
-                args{3} = varargin{3};
-                args{4} = varargin{4};
             else
                 args = {};
             end
@@ -49,9 +37,7 @@ classdef SystemBehavior < Behavior
             obj = obj@Behavior(args{:});
             % post-initialization: set class-specific values
             if nargin==5
-                obj.id = varargin{1};
-            elseif nargin==4
-                obj.id = CityNet.instance().GetNextSystemBehaviorId();
+                obj.system = varargin{1};
             else
                 obj.id = CityNet.instance().GetNextSystemBehaviorId();
                 obj.name = ['System Behavior ' num2str(obj.id)];
@@ -60,14 +46,64 @@ classdef SystemBehavior < Behavior
                 obj.bounds = '[0,0]';
             end
         end
-    end
-    methods(Abstract)
+        
         %% Evaluate Function
-        % Evaluates the behavior and returns the computed value.
+        % Evaluates the behavior, updates the stored copy, and returns the
+        % computed value.
         %
         % val = obj.Evaluate()
         %   obj:    the behavior to evaluate
         %   val:    the evaluated value
-        val = Evaluate(obj)
+        function val = Evaluate(obj)
+            val = obj.EvaluateImpl();
+            obj.value = val;
+        end
+    end
+    methods(Abstract)
+        %% EvaluateImpl Function
+        % Implementation that evaluates the behavior and returns the value.
+        %
+        % val = obj.Evaluate()
+        %   obj:    the behavior to evaluate
+        %   val:    the evaluated value
+        val = EvaluateImpl(obj)
+    end
+    methods(Access=protected)
+        %% SumNodeAttributes Function
+        % Supplied function that sums all node attributes of a particular
+        % name within the system.
+        %
+        % val = obj.SumNodeAttributes(attributeName)
+        %   val:            the sum of all node attributes
+        %   obj:            the system behavior handle
+        %   attributeName:  the attribute name for which to sum
+        function val = SumNodeAttributes(obj,attributeName)
+            val = 0;
+            for i=1:length(obj.system.nodes)
+                node = obj.system.nodes(i);
+                if sum(strcmp({node.type.attributes.name},attributeName))==1
+                    val = val + node.type.attributes(strcmp({node.type.attributes.name},attributeName)).value;
+                end
+            end
+        end
+        
+        %% SumDensityNodeAttributes Function
+        % Supplied function that sums all node attributes multiplied by 
+        % cell area of a particular name within the system.
+        %
+        % val = obj.SumDensityNodeAttributes(attributeName)
+        %   val:            the sum of all node attributes
+        %   obj:            the system behavior handle
+        %   attributeName:  the attribute name for which to sum
+        function val = SumDensityNodeAttributes(obj,attributeName)
+            val = 0;
+            for i=1:length(obj.system.nodes)
+                node = obj.system.nodes(i);
+                if sum(strcmp({node.type.attributes.name},attributeName))==1
+                    val = val + node.cell.GetArea()*node.type.attributes( ...
+                        strcmp({node.type.attributes.name},attributeName)).value;
+                end
+            end
+        end
     end
 end
