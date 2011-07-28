18d9c9390ba3f0a17130f141fff783f09f9d47c0
Paul T. Grogan <pgrogan@stevens.edu>
2011-07-28 00:46:28 +0000

    added save menu items to menu bar

--- java/src/edu/mit/citynet/gui/CityNetMenuBar.java
+++ java/src/edu/mit/citynet/gui/CityNetMenuBar.java
@@ -1,5 +1,6 @@
 package edu.mit.citynet.gui;
 
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -18,10 +19,19 @@ import javax.swing.KeyStroke;
  */
 public class CityNetMenuBar extends JMenuBar {
 	private static final long serialVersionUID = -6018553717087352056L;
+
+	CityNetFrame frame;
 	JMenu fileMenu;
-	JMenuItem newCityItem, openCityItem, closeCityItem, exitItem;
+	JMenuItem newCityItem, openCityItem, closeCityItem, saveCityAsItem, 
+		saveCityItem, exitItem;
 	
-	public CityNetMenuBar() {
+	/**
+	 * Instantiates a new city net menu bar.
+	 *
+	 * @param frame the City.Net frame
+	 */
+	public CityNetMenuBar(final CityNetFrame frame) {
+		this.frame = frame;
 	    fileMenu = new JMenu("File");
 		fileMenu.setMnemonic(KeyEvent.VK_F);
 	    newCityItem = new JMenuItem("New City", KeyEvent.VK_N);
@@ -29,9 +39,7 @@ public class CityNetMenuBar extends JMenuBar {
                 KeyEvent.VK_N, ActionEvent.CTRL_MASK));
 	    newCityItem.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent e) {
-        		if(getTopLevelAncestor() instanceof CityNetFrame) {
-        			((CityNetFrame)getTopLevelAncestor()).newCityCommand();
-        		}
+    			frame.newCityCommand();
         	}
         });
         fileMenu.add(newCityItem);
@@ -40,12 +48,11 @@ public class CityNetMenuBar extends JMenuBar {
                 KeyEvent.VK_O, ActionEvent.CTRL_MASK));
         openCityItem.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent e) {
-        		if(getTopLevelAncestor() instanceof CityNetFrame) {
-        			((CityNetFrame)getTopLevelAncestor()).openCityCommand();
-        		}
+    			frame.openCityCommand();
         	}
         });
         fileMenu.add(openCityItem);
+        fileMenu.addSeparator();
         closeCityItem = new JMenuItem("Close City", KeyEvent.VK_C);
         closeCityItem.setAccelerator(KeyStroke.getKeyStroke(
                 KeyEvent.VK_W, ActionEvent.CTRL_MASK));
@@ -57,17 +64,45 @@ public class CityNetMenuBar extends JMenuBar {
         	}
         });
         fileMenu.add(closeCityItem);
+        fileMenu.addSeparator();
+        saveCityItem = new JMenuItem("Save City", KeyEvent.VK_S);
+        saveCityItem.setAccelerator(KeyStroke.getKeyStroke(
+                KeyEvent.VK_S, ActionEvent.CTRL_MASK));
+        saveCityItem.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+    			frame.saveCityCommand();
+        	}
+        });
+        fileMenu.add(saveCityItem);
+        saveCityAsItem = new JMenuItem("Save City as...", KeyEvent.VK_A);
+        saveCityAsItem.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+    			frame.saveCityAsCommand();
+        	}
+        });
+        fileMenu.add(saveCityAsItem);
+        fileMenu.addSeparator();
         exitItem = new JMenuItem("Exit");
         exitItem.setAccelerator(KeyStroke.getKeyStroke(
                 KeyEvent.VK_Q, ActionEvent.CTRL_MASK));
         exitItem.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent e) {
-        		if(getTopLevelAncestor() instanceof CityNetFrame) {
-        			((CityNetFrame)getTopLevelAncestor()).exitCommand();
-        		}
+    			frame.exitCommand();
         	}
         });
         fileMenu.add(exitItem);
 	    add(fileMenu);
+	}
+	
+	/* (non-Javadoc)
+	 * @see javax.swing.JComponent#paint(java.awt.Graphics)
+	 */
+	public void paint(Graphics g) {
+		super.paint(g);
+		newCityItem.setEnabled(!frame.isCityOpen());
+		openCityItem.setEnabled(!frame.isCityOpen());
+		closeCityItem.setEnabled(frame.isCityOpen());
+		saveCityItem.setEnabled(frame.isCityOpen());
+		saveCityAsItem.setEnabled(frame.isCityOpen());
 	}
 }
