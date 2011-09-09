a8f4d9587f4a32bac6e7c4fc34660d281acdf930
Afreen Siddiqi <siddiqi@mit.edu>
2011-09-09 15:25:31 +0000

    Added code for plotting results

--- WaterSystem/ExecuteWaterSystem.m
+++ WaterSystem/ExecuteWaterSystem.m
@@ -40,3 +40,79 @@ SWROplant.Evaluate();
 
 MBRplant = MBRfacility();
 MBRplant.Evaluate();
+
+%plot outputs
+figure
+subplot(2,2,1)
+bar([resWater.kitchen, resWater.faucets, resWater.shower, resWater.toilets, resWater.laundry],'c')
+title(strcat('Annual Residential Water Demand: ',num2str(resWater.value),' m^3'))
+ylabel('m^3')
+set(gca,'XTickLabel',{'kitchen','faucets','shower','toilets','laundry'})
+grid on
+
+subplot(2,2,3)
+bar([resWasteWater.greywater, resWasteWater.blackwater],0.5,'b')
+title('Annual Residential Waste Water Generation')
+ylabel('m^3')
+set(gca,'XTickLabel',{'grey','black'})
+grid on
+
+subplot(2,2,2)
+bar([comWater.kitchen, comWater.faucets, comWater.toilets],'c')
+title(strcat('Annual Commercial Water Demand: ',num2str(comWater.value),' m^3'))
+ylabel('m^3')
+set(gca,'XTickLabel',{'kitchen','faucets','toilets'})
+grid on
+
+subplot(2,2,4)
+bar([comWasteWater.greywater, comWasteWater.blackwater],0.5,'b')
+title('Annual Commercial Waste Water Generation')
+ylabel('m^3')
+set(gca,'XTickLabel',{'grey','black'})
+grid on
+
+figure
+subplot(2,1,1)
+%blue water, water demand
+bar([resWater.kitchen, resWater.faucets, resWater.shower, resWater.toilets, resWater.laundry;...
+    comWater.kitchen, comWater.faucets, 0, comWater.toilets, 0]','grouped')
+title('Water demand')
+ylabel('m^3')
+grid on
+legend('residential','commercial')
+set(gca,'XTickLabel',{'kitchen','faucets','shower','toilets','laundry'})
+
+subplot(2,1,2)
+bar([resWasteWater.greywater, resWasteWater.blackwater;...
+    comWasteWater.greywater, comWasteWater.blackwater]','grouped')
+title('Waste Water Generation')
+grid on
+ylabel('m^3')
+legend('residential','commercial')
+set(gca,'XTickLabel',{'grey','black'})
+grid on
+
+
+
+
+figure
+subplot(2,2,1)
+bar([SWROplant.electricEnergy, MBRplant.electricEnergy],0.5,'r')
+title('water facilities annual electric energy use')
+ylabel('kWh')
+set(gca,'XTickLabel',{'SWRO plant','MBR plant'})
+grid on
+
+subplot(2,2,2)
+bar([SWROplant.landfootprint, MBRplant.landfootprint],0.5,'g')
+title('water facilities land use')
+ylabel('m^2')
+set(gca,'XTickLabel',{'SWRO plant','MBR plant'})
+grid on
+
+subplot(2,2,3)
+bar([SWROplant.costOfProducedWater, MBRplant.costOfTreatedWater],0.5,'k')
+title('water costs per unit volume')
+ylabel('$/m^3')
+set(gca,'XTickLabel',{'SWRO plant','MBR plant'})
+grid on
